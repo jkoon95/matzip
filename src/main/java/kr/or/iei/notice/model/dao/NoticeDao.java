9cb8267ea24c333776a72522df02df9c2de29091
jkoon95 <jkoon106@gmail.com>
2024-02-19 18:04:42 +0900

    공지작성까지

--- src/main/java/kr/or/iei/notice/model/dao/NoticeDao.java
+++ src/main/java/kr/or/iei/notice/model/dao/NoticeDao.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.or.iei.notice.model.dto.Notice;
+import kr.or.iei.notice.model.dto.NoticeFile;
+import kr.or.iei.notice.model.dto.NoticeFileRowMapper;
 import kr.or.iei.notice.model.dto.NoticeRowMapper;
 
 @Repository
@@ -14,6 +17,9 @@ public class NoticeDao {
 	private JdbcTemplate jdbc;
 	@Autowired
 	private NoticeRowMapper noticeRowMapper;
+	@Autowired
+	private NoticeFileRowMapper noticeFileRowMapper;
+	
 	
 	public List selectNoticeList(int start, int end) {
 		String query = "select * from (select rownum as rnum, n.* from (select * from notice_tbl order by 1 desc)n) where rnum between ? and ?";
@@ -26,6 +32,26 @@ public class NoticeDao {
 		String query = "select count(*) from notice_tbl";
 		int totalCount = jdbc.queryForObject(query, Integer.class);
 		return totalCount;
+	}
+
+	public int insertNotice(Notice n) {
+		String query = "insert into notice_tbl values(notice_seq.nextval,?,?,?,0,to_char(sysdate,'yyyy-mm-dd'))";
+		Object[] params = {n.getNoticeTitle(),n.getNoticeWriter(),n.getNoticeContent()};
+		int result = jdbc.update(query,params);
+		return result;
+	}
+
+	public int insertNoticeFile(NoticeFile noticeFile) {
+		String query = "insert into notice_file values(notice_file_seq.nextval,?,?,?)";
+		Object[] params = {noticeFile.getNoticeNo(),noticeFile.getFilename(),noticeFile.getFilepath()};
+		int result = jdbc.update(query,params);
+		return result;
+	}
+
+	public int selectNoticeNo() {
+		String query = "select max(notice_no) from notice_tbl";
+		int noticeNo = jdbc.queryForObject(query, Integer.class);
+		return noticeNo;
 	}
 	
 }
