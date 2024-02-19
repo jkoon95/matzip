6e53769d98af9127ba80eaf012e29b530f69431c
qkrqkrtm <mp2221mp2221@gmail.com>
2024-02-19 13:56:52 +0900

    스토어 진행1

--- src/main/java/kr/or/iei/store/model/dao/StoreDao.java
+++ src/main/java/kr/or/iei/store/model/dao/StoreDao.java
@@ -1,5 +1,15 @@
 package kr.or.iei.store.model.dao;
 
-public class StoreDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import kr.or.iei.store.model.dto.StoreRowMapper;
+
+@Repository
+public class StoreDao {
+	@Autowired
+	private JdbcTemplate jdbc;
+	@Autowired
+	private StoreRowMapper storeRowMapper;
 }
