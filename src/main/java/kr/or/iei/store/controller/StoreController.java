7b6131c5b6365696f0e09598f514944a2b8920f7
dltkfkd92 <lsr992@naver.com>
2024-02-21 14:36:03 +0900

    매장등록10

--- src/main/java/kr/or/iei/store/controller/StoreController.java
+++ src/main/java/kr/or/iei/store/controller/StoreController.java
@@ -1,5 +1,6 @@
 package kr.or.iei.store.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.iei.FileUtils;
 import kr.or.iei.store.model.dto.EvidenceFile;
+import kr.or.iei.store.model.dto.Menu;
 import kr.or.iei.store.model.dto.Store;
 import kr.or.iei.store.model.service.StoreService;
 
@@ -40,13 +42,53 @@ public class StoreController {
 		}
 		
 		@PostMapping(value="/storeEnroll")
-		public String storeEnroll(Store store, MultipartFile[] edvienceFile, String address,String detailAddress, String[] closedDays, MultipartFile storeImgFile, MultipartFile[] menuImgFile, String[] name, int[] price, Model model) {
+		public String storeEnroll(Store store, MultipartFile[] edvienceUpFile, String address,String detailAddress, String[] closedDays, MultipartFile storeImgFile, MultipartFile[] menuImgFile, String[] name, int[] price, Model model) {
 			
+			//store 값 설정
 			store.setStoreAddr(address+" "+detailAddress);	//도로명 + 상세주소
-			
 			String storeSavepath = root+"/store/";	//첨부파일이 무조건 있다는 가정하에(검사안함)
 			String storeFilepath = fileUtils.upload(storeSavepath, storeImgFile);
 			store.setStoreImg(storeFilepath);
+			
+			//사업자증빙자료
+			List<EvidenceFile> evidenceFileList = new ArrayList<EvidenceFile>();
+			if(!edvienceUpFile[0].isEmpty()) { 
+				String evidenceSavepath = root+"/evidence/";	//-> 웹컨피그가서 값설정해줘야함 아직안함
+				for(MultipartFile file : edvienceUpFile) {
+					//업로드한 파일명을 추출
+					String evidenceFilename = file.getOriginalFilename();
+					String evidenceFilepath = fileUtils.upload(evidenceSavepath, file);
+					EvidenceFile evidenceFile = new EvidenceFile();
+					evidenceFile.setFilename(evidenceFilename);
+					evidenceFile.setFilepath(evidenceFilepath);
+					evidenceFileList.add(evidenceFile);
+				}
+			}
+			
+			//휴무일 날짜
+			//휴무일 배열 보냄
+			//휴무일 널체크 해줘야함??????????????하나라도 안들어올시 for length에서 에러
+			//closedDays
+			
+			
+			//메뉴 등록값
+			List<Menu> menuList = new ArrayList<Menu>();
+			String menuSavepath = root+"/menu/";		//->얘도 웹컨피그가서 해줘야댐
+			for(int i=0; i<name.length; i++) {
+				Menu menu = new Menu();
+				String menuFilepath = fileUtils.upload(menuSavepath, menuImgFile[i]);				
+				String menuName = name[i];
+				int menuPrice = price[i];
+				menu.setMenuImg(menuFilepath);
+				menu.setMenuName(menuName);
+				menu.setMenuPrice(menuPrice);
+				menuList.add(menu);
+			}
+			
+			
+			
+			
+			//성공갯수 구해야댐
 			
 			
 			/*데이터 확인용
