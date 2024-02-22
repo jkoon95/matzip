94a67020272ef2af192c28482e085ba78cc7bcee
swanpark8538 <swanpark8538@gmail.com>
2024-02-22 18:05:31 +0900

    datepicker

--- src/main/java/kr/or/iei/reserve/model/service/ReserveService.java
+++ src/main/java/kr/or/iei/reserve/model/service/ReserveService.java
@@ -1,6 +1,5 @@
 package kr.or.iei.reserve.model.service;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,7 +9,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.or.iei.member.model.dto.Member;
 import kr.or.iei.reserve.model.dao.ReserveDao;
 import kr.or.iei.reseve.model.dto.TempClosedDay;
 import kr.or.iei.store.model.dto.ClosedDay;
@@ -157,6 +155,48 @@ public class ReserveService {
 			tempClosedDays.add(item.getTempClosedDay());
 		}
 		return tempClosedDays;
+	}
+
+	public HashMap<String, List<String>> remainTimes(Store store, HashMap<String, List<String>> fullTimes) {
+		//00:00부터 openingHour까지 30분 단위로 예약 가능한 횟수
+		int openingCount = (Integer.parseInt(store.getOpeningHour().substring(0,2)) * 2) + (Integer.parseInt(store.getOpeningHour().substring(3,5)) / 30);
+		//00:00부터 closingHour까지 30분 단위로 예약 가능한 횟수
+		int closingCount = (Integer.parseInt(store.getClosingHour().substring(0,2)) * 2) + (Integer.parseInt(store.getClosingHour().substring(3,5)) / 30);
+		//00:00부터 breakStart, breakEnd 까지 30분 단위로 예약 가능한 횟수(휴게시간 없다면 0으로 되도록 세팅)
+		int breakStartCount = 0;
+		int breakEndCount = 0;
+		if(store.getBreakStart() != "-1") { //사장이 breakStart를 선택 안 하면 기본 value가 문자열 "-1"이 되도록 세팅해놨음. 즉 휴게시간이 없을떄 -1임.
+			breakStartCount = (Integer.parseInt(store.getBreakStart().substring(0,2)) * 2) + (Integer.parseInt(store.getBreakStart().substring(3,5)) / 30);
+			breakEndCount = (Integer.parseInt(store.getBreakEnd().substring(0,2)) * 2) + (Integer.parseInt(store.getBreakEnd().substring(3,5)) / 30);
+		};
+		if(openingCount < closingCount) {//당일 열고 당일 닫는 경우 또는 24시간 운영하는 경우
+			//if(store.getBreakStart() != "-1") {//휴게시간이 있는 경우
+				//dayTotalCount = (closingCount - breakEndCount) + (breakStartCount - openingCount);
+			//}else {//휴게시간이 없는 경우
+				//dayTotalCount = closingCount - openingCount;
+			//}
+			//계산해보니 휴게시간이 없으면 breakCount == 0 이어서, 더하나 빼나 상관 없네? 휴게시간 있는 경우와 없는 경우 모두 하나의 수식으로 합치자 ↓
+			dayTotalCount = closingCount - breakEndCount + breakStartCount - openingCount;
+		}else {//당일 열고 익일 닫는 경우
+			if(store.getBreakStart() != "-1") {//휴게시간이 있는 경우
+				if(breakStartCount < breakEndCount) { //휴게시간 시작과 종료가 모두 같은 날인 경우
+					//if(openingCount < breakStartCount) { //휴게시간 시작과 종료가 opening날짜와 같은 경우(당일인 경우)
+						//dayTotalCount = (48 - breakEndCount) + (breakStartCount - openingCount) + (closingCount - 0);
+					//}else {//휴게시간 시작과 종료가 closing날짜와 같은 경우(익일인 경우)
+						//dayTotalCount = (48 - openingCount) + (closingCount - breakEndCount) + (breakStartCount - 0);
+					//}
+					//계산해보니 위 두 경우의 수의 수식이 똑같아서, 하나의 수식으로 합치자 ↓
+					dayTotalCount = 48 + closingCount - openingCount - breakEndCount + breakStartCount;
+				}else { //휴게시간 시작은 당일이고 종료는 익일인 경우(breakStart > breakEnd)
+					dayTotalCount = (breakStartCount - openingCount) + (closingCount - breakEndCount);
+				}
+			}else {//휴게시간이 없는 경우
+				dayTotalCount = 48 + closingCount - openingCount;
+			}
+		};
+		
+		
+		return remainTimes;
 	}
 
 	
