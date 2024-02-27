37bb5ecee61c587b9777eddf5020edabb23fae4c
swanpark8538 <swanpark8538@gmail.com>
2024-02-27 23:39:54 +0900

    reserveList

---
+++ src/main/resources/static/js/reserveList.js
@@ -0,0 +1,28 @@
+/*
+HashMap<String, List<ReserveViewMember>> rvmList
+key : after , value : afterRvmList  ( = List<ReserveViewMember> )
+key : before, value : beforeRvmList ( = List<ReserveViewMember> )
+*/
+
+/*
+afterRvmList = {
+  <key>           :   <value>
+  reserveNo       :
+	reserveDate     :
+	reserveTime     :
+	reservePeople   :
+	reserveRequest  :
+	storeNo         :
+	tableNo         :
+	
+	
+  storeName       :
+	storeImg        :
+	
+	servings        :
+	
+	menuName        :
+}
+
+beforeRvmList도 마찬가지
+*/
